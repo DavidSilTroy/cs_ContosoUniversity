d35872daec88ad030a8b396fcb57bc84152b8bbe
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-06 17:01:04 +0200

    Migration done by the browser but also by the Package Manager Console using the command Update-Database

---
+++ Areas/Admin/Controllers/MaintainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoUniversity.Areas.Admin.Controllers
+{
+    public class MaintainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
