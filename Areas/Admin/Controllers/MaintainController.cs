171bd124f9055b274833d7613bbf2e9609c2a53f
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-06 17:22:21 +0200

    Authorization set up

--- Areas/Admin/Controllers/MaintainController.cs
+++ Areas/Admin/Controllers/MaintainController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoUniversity.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
     public class MaintainController : Controller
     {
         public IActionResult Index()
--- Startup.cs
+++ Startup.cs
@@ -38,9 +38,16 @@ namespace ContosoUniversity
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddControllersWithViews();
+
+            services.AddIdentity<IdentityUser, IdentityRole>() //<-------
+                .AddRoleManager<RoleManager<IdentityRole>>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
