1c4a0d110d8da8f8c05e4fd8359fdc2c53242607
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-06 17:41:26 +0200

    Deleting the Maintaince controller and view, not necessary anymore

--- Areas/Admin/Controllers/StudentsController.cs
+++ Areas/Admin/Controllers/StudentsController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ContosoUniversity.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
     public class StudentsController : Controller
     {
         private readonly SchoolContext _context;
